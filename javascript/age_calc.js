96494cc63b328ecb65e57b82a8ea099bdca43cfe
Kunal Katarya <kunal.rox@gmail.com>
2020-08-12 11:14:48 +0800

    started js. need to blast throughgit statusgit status

---
+++ javascript/age_calc.js
@@ -0,0 +1,4 @@
+const age = prompt("What is your age in years?");
+const age_days = age*365.25;
+alert(age + " in years is " + age_days + " in days! You old, bitch!!");
+console.log("You old, bitch!!");

00a246b90029d71eb3970020f2b4f3e96bac88ea
Kunal Katarya <kunal.rox@gmail.com>
2020-10-06 15:05:10 +0800

    ejs

---
+++ more_express/app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+app = express();
+
+app.get("/", function(req, res){
+  res.render("home.ejs");
+});
+
+app.get("/fallinlovewith/:thing", function(req,res){
+  var thing = req.params.thing;
+  res.render("love.ejs", {thingVar: thing})
+});
+
+
+
+app.listen(3000, function(){
+  console.log("server is listening");
+});
